f3c617d349964fc90b9068ea5c3f8cd330311a76
HeavyPunk <kirillkuchma8@gmail.com>
2021-11-20 14:17:33 +0500

    Initial commit

---
+++ src/main/java/com/zaordu/servicestorage/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.zaordu.servicestorage.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/greeting")
+    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
