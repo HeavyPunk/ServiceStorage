b16848e8de7ab8a5ce49149bff9a62dcaa9b6a97
1shaman1 <narxq@yandex.ru>
2021-11-20 17:53:32 +0500

    сделал хуйню

--- src/main/java/com/zaordu/servicestorage/controllers/BDWorker.java
+++ src/main/java/com/zaordu/servicestorage/controllers/BDWorker.java
@@ -1,5 +1,14 @@
 package com.zaordu.servicestorage.controllers;
+import java.sql.SQLException;
 
 public class BDWorker {
+    private static final String ConnectionStrDB = "jdbc:sqlite:services.db";
 
+    private static BDWorker instance = null;
+
+    public static synchronized BDWorker getInstance() throws SQLException {
+        if (instance == null)
+            instance = new BDWorker();
+        return instance;
+    }
 }
