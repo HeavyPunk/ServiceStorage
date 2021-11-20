bd3b5275173238ecf332f421543ec5a9814e145b
HeavyPunk <kirillkuchma8@gmail.com>
2021-11-20 21:56:53 +0500

    Добавил возможность получения информации о сервисе

--- src/main/java/com/zaordu/servicestorage/abstractions/ServiceHandler.java
+++ src/main/java/com/zaordu/servicestorage/abstractions/ServiceHandler.java
@@ -3,9 +3,11 @@ package com.zaordu.servicestorage.abstractions;
 import com.zaordu.servicestorage.models.ServiceModel;
 
 import java.util.Set;
+import java.util.UUID;
 
 public interface ServiceHandler {
     Set<ServiceModel> getServicesInfo();
     void addService(ServiceModel newService);
     void rewriteService(ServiceModel newModel);
+    ServiceModel getServiceInfo(UUID id);
 }
